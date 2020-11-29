8884e824fb4bc227b9bab38bb9bb6d7579be4ecf
LenardPiir <37505458+LenardPiir@users.noreply.github.com>
2020-11-29 16:33:41 +0200

    repair websocket url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ class App extends Component{
 			]
 		}
 		
-		let socket = new WebSocket("ws://localhost:3000/results-api/myHandler");
+		let socket = new WebSocket("ws://" + location.host + "/results-api/myHandler");
 		window.a = socket;
 
 		socket.onopen = function(e) {
